abc3c917d89d973d74a9a65c6b824e4930fc896a
Woojong Kim <woojong92@gmail.com>
2019-04-30 16:20:02 +0900

    2.3 Changing Color

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor")
 
-ctx.strokeStyle = "black";
+//캔버스의 픽섹사이즈 pixel modifier
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
 
 let painting = false;
-
-
 
 function startPainting(){
     painting = true;
@@ -20,10 +23,13 @@ function onMouseMove(event){
     const x = event.offsetX;
     const y = event.offsetY;
     if(!painting){
-        ctx.beginPath();
+        ctx.beginPath(); // path
         ctx.moveTo(x, y);
+    } else{
+        ctx.lineTo(x, y);
+        ctx.stroke();
     }
-    console.log(x, y);
+    //console.log(x, y);
 }
 
 function onMouseDown(event){
@@ -32,7 +38,11 @@ function onMouseDown(event){
     console.log(event);
 }
 
-
+function handleColorClick(event){
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+    console.log(color);
+}
 
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
@@ -40,3 +50,5 @@ if(canvas){
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
+
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
