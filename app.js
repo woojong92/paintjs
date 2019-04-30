54eb9f6a8f6c098babce6cb87356386e3b140a0e
Woojong Kim <woojong92@gmail.com>
2019-04-30 22:36:46 +0900

    2.5 Filling Mode

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor")
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+
+const INITIAL_COLOR = "#2c2c2c"
 
 //캔버스의 픽섹사이즈 pixel modifier
 canvas.width = 700;
 canvas.height = 700;
 
-ctx.strokeStyle = "#2c2c2c";
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
+
+
 let painting = false;
+let filling = false;
 
 function startPainting(){
     painting = true;
@@ -41,7 +49,14 @@ function onMouseDown(event){
 function handleColorClick(event){
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
     console.log(color);
+}
+
+function handleCanvasClick(){
+    if(filling){
+        ctx.fillRect(0, 0, 700, 700);
+    }
 }
 
 if(canvas){
@@ -49,6 +64,33 @@ if(canvas){
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", handleCanvasClick);
 }
 
 Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
+
+function handleRangeChange(event){
+    console.log(event);
+    const size = event.target.value;
+    ctx.lineWidth = size;
+}
+
+function handleModeclick() {
+    if(filling === true ) {
+        filling = false;
+        mode.innerText = "Fill"
+    }else {
+        filling = true;
+        mode.innerText = "Paint"
+        
+        
+    }
+}
+
+if(range){
+    range.addEventListener("input", handleRangeChange);
+}
+
+if(mode) {
+    mode.addEventListener("click", handleModeclick);
+}
